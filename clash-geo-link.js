01aa431316ad63173fff6baf4443845f47cfcada
senhao-xu <xusenhao1123@163.com>
2025-08-28 22:35:44 +0800

    update code.

--- clash-geo-link.js
+++ clash-geo-link.js
@@ -165,6 +165,7 @@ const rules = [
   'GEOSITE,github,Proxy',
   'GEOSITE,twitter,Proxy',
   'GEOSITE,pixiv,Proxy',
+  'GEOSITE,cursor,OpenAi',
   'GEOSITE,category-scholar-!cn,Proxy',
   'GEOSITE,category-ai-!cn,OpenAi',
   'GEOSITE,discord,Proxy',
