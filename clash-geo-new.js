4cf2e89ee9d078e2ddb6033315abe4a087314bd0
Simon <47684791+senhao-xu@users.noreply.github.com>
2026-06-15 10:28:16 +0800

    Update clash-geo-new.js

--- clash-geo-new.js
+++ clash-geo-new.js
@@ -12,29 +12,39 @@ function main(config) {
   if (proxyCount === 0 && proxyProviderCount === 0) {
     throw new Error("配置文件中未找到任何代理");
   }
-  // 合并而非覆盖
+
   config["proxy-providers"] = {
-    ...originalProviders,  // 保留原有配置
+    ...originalProviders,
   };
-  // 覆盖原配置中DNS配置
+
   config["dns"] = dnsConfig;
-  // 覆盖原配置中的代理组
   config["proxy-groups"] = proxyGroupConfig;
-  // 覆盖原配置中的规则
   config["rule-providers"] = ruleProviders;
   config["rules"] = rules;
-  //覆盖通用配置
+
   config["mixed-port"] = 7890;
   config["allow-lan"] = true;
   config["bind-address"] = "*";
+  config["mode"] = "rule";
   config["ipv6"] = false;
   config["unified-delay"] = true;
   config["tcp-concurrent"] = true;
   config["external-controller"] = "9090";
-  // 返回修改后的配置
+
+  config["profile"] = {
+    ...(config["profile"] || {}),
+    "store-selected": true,
+    "store-fake-ip": true,
+  };
+
+  config["tun"] = {
+    ...(config["tun"] || {}),
+    "dns-hijack": config?.tun?.["dns-hijack"] || ["any:53"],
+  };
+
   return config;
 }
-// DNS配置
+
 const dnsConfig = {
   "enable": true,
   "listen": "0.0.0.0:1053",
@@ -48,40 +58,43 @@ const dnsConfig = {
   "enhanced-mode": "fake-ip",
   "fake-ip-range": "198.18.0.1/16",
   "fake-ip-filter": [
+    "*.lan",
+    "*.local",
+    "localhost.ptlogin2.qq.com",
+    "*.msftconnecttest.com",
+    "*.msftncsi.com",
     "*.senhao.xyz",
     "*.112320.xyz",
     "geosite:private",
-    "geosite:cn",
-    "geosite:geolocation-!cn"
+    "geosite:cn"
   ],
   "default-nameserver": [
     "223.5.5.5",
     "119.29.29.29"
   ],
-// 默认的域名解析服务器，如不配置 fallback/proxy-server-nameserver , 则所有域名都由 nameserver 解析
   "nameserver": [
-    "https://cloudflare-dns.com/dns-query#Proxy", // CloudflareDNS
-    "https://8.8.8.8/dns-query#Proxy" // Google公共DNS
+    "https://cloudflare-dns.com/dns-query#Proxy",
+    "https://8.8.8.8/dns-query#Proxy"
   ],
-// 代理节点域名解析服务器，仅用于解析代理节点的域名
   "proxy-server-nameserver": [
-    "https://223.5.5.5/dns-query", // 阿里DoH
-    "https://doh.pub/dns-query" // 腾讯DoH
+    "https://223.5.5.5/dns-query",
+    "https://doh.pub/dns-query"
   ],
   "direct-nameserver": [
-    "https://223.5.5.5/dns-query", // 阿里DoH
-    "https://doh.pub/dns-query" // 腾讯DoH
+    "https://223.5.5.5/dns-query",
+    "https://doh.pub/dns-query"
   ],
-// 指定域名查询的解析服务器，可使用 geosite, 优先于 nameserver/fallback 查询
   "direct-nameserver-follow-policy": false,
   "nameserver-policy": {
-    'geosite:cn': [
+    "geosite:cn": [
       "https://223.5.5.5/dns-query",
       "https://doh.pub/dns-query"
+    ],
+    "geosite:private": [
+      "system"
     ]
   }
 };
-
 
 // 代理组规则
 const proxyGroupConfig = [
